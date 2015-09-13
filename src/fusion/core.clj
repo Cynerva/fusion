b20ad166eabd585c0168064d59a5909c3b15fc53
George Kraft <cynerva@gmail.com>
2015-09-13 18:30:42 -0500

    Make fuse-changes test green

--- src/fusion/core.clj
+++ src/fusion/core.clj
@@ -1,3 +1,22 @@
 (ns fusion.core)
 
-(def fuse atom)
+(defn watch [ref f]
+  (add-watch ref f (fn [_ _ _ _] (f))))
+
+(defn replace-derefs [watch-fn-sym expr]
+  (cond
+    (seq? expr) (if (= `~(first expr) `deref)
+                  `(let [ref# ~(replace-derefs watch-fn-sym (second expr))]
+                     (watch ref# ~watch-fn-sym)
+                     (deref ref# ~@(map (partial replace-derefs watch-fn-sym)
+                                        (drop 2 expr))))
+                    ~@(map (partial replace-derefs watch-fn-sym) expr))
+    :else expr))
+
+(defmacro fuse [& body]
+  (let [watch-fn-sym (gensym "watch-fn")]
+    `(let [fused# (atom nil)]
+       ((fn ~watch-fn-sym []
+          (reset! fused# (do ~@(map (partial replace-derefs watch-fn-sym)
+                                    body)))))
+       fused#)))
